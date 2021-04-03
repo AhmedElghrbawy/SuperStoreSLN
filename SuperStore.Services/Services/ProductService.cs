73552c3329fe939fcebc6014ee6a4d1b85a65f3e
Ahmed Elghrbawy <ahmedelghrbawy420@gmail.com>
2021-04-03 19:30:41 +0200

    Set product owner in product service

--- SuperStore.Services/Services/ProductService.cs
+++ SuperStore.Services/Services/ProductService.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using SuperStore.Data;
 using SuperStore.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +14,12 @@ namespace SuperStore.Services.Services
     public class ProductService
     {
         private readonly StoreDBContext _dbContext;
+        private readonly UserManager<User> _userManager;
 
-        public ProductService(StoreDBContext dbContext)
+        public ProductService(StoreDBContext dbContext, UserManager<User> userManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
         }
 
         public async Task<Product> GetProductByIdAsync(int id)
@@ -29,13 +33,18 @@ namespace SuperStore.Services.Services
         }
 
         
-        public async Task<Product> AddProductAsync(Product product)
+        public async Task<Product> AddProductAsync(Product product, ClaimsPrincipal UserClaim)
         {
+            var user = await _userManager.GetUserAsync(UserClaim);
             product.ViewCount = 0;
-            /*  
-             *    Check for current User here
+            product.OwnerId = user.Id;
+
+
+            /* TODO
              * 
+             * set Product image here or in view
              */
+
 
             _dbContext.Add(product);
 
