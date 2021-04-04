f44d599f1dca8eed2d5cd17046b99ad6cec169fb
Ahmed Elghrbawy <ahmedelghrbawy420@gmail.com>
2021-04-04 16:37:35 +0200

    Product Creation done, product partial view added

--- SuperStore/Controllers/ProductController.cs
+++ SuperStore/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using SuperStore.Data.Models;
 using SuperStore.Services.Services;
 using SuperStore.Web.Models;
 using System;
@@ -12,14 +13,31 @@ namespace SuperStore.Web.Controllers
     public class ProductController : Controller
     {
         private readonly CategoryService _categoryService;
+        private readonly ProductService _productService;
 
-        public ProductController(CategoryService categoryService)
+        public ProductController(CategoryService categoryService, ProductService productService)
         {
             _categoryService = categoryService;
+            _productService = productService;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = await _productService.GetProductsAsync();
+
+            var productViewModels = products.Select(p => new ProductViewModel
+            {
+                AmountAvailable = p.AmountAvailable,
+                Title = p.Title,
+                Description = p.Description,
+                CategoryId = p.CategoryId,
+                ImageData = p.Image,
+                Id = p.Id,
+                OwnerId = p.OwnerId,
+                Price = p.Price,
+                Reviews = p.Reviews
+            });
+
+            return View(productViewModels);
         }
 
 
@@ -33,11 +51,28 @@ namespace SuperStore.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(ProductViewModel productViewModel)
+        public async Task<IActionResult> Create(ProductViewModel productViewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(productViewModel);
+            }
             productViewModel.ImageFormFile = Request.Form.Files["ImageFile"];
+            var productModel = new Product
+            {
+                Title = productViewModel.Title,
+                Description = productViewModel.Description,
+                AmountAvailable = productViewModel.AmountAvailable,
+                CategoryId = productViewModel.CategoryId,
+                Image = productViewModel.ImageData,
+                Price = productViewModel.Price,
+            };
 
-            return View("Index");
+            await _productService.CreateProductAsync(productModel, User);
+
+            
+
+            return RedirectToAction("Index");
         }
     }
 }
--- SuperStore/Startup.cs
+++ SuperStore/Startup.cs
@@ -40,6 +40,8 @@ namespace SuperStore
                 .AddDefaultUI();
             services.AddRazorPages();
             services.AddTransient<CategoryService>();
+            services.AddTransient<ProductService>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
