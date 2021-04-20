34e95cf2efb4b863290e054dbf318d2a4f2da181
Ahmed Elghrbawy <ahmedelghrbawy420@gmail.com>
2021-04-20 03:04:11 +0200

    Review functionality demo added

---
+++ SuperStore/wwwroot/js/Review.js
@@ -0,0 +1,25 @@
+﻿
+
+
+let starContainer = document.getElementsByClassName("stars-container-test")[0];
+
+
+starContainer.addEventListener("click", modifyReviewStars);
+console.log(starContainer);
+
+function modifyReviewStars(event) {
+    var starNumber = event.target.dataset.star_number;
+    if (starNumber == undefined) {
+        return;
+    }
+    let starGroup = starContainer.querySelectorAll(".review-star");
+    for (let i = 0; i < starNumber; i++) {
+        starGroup[i].classList.remove("far");
+        starGroup[i].classList.add("fas");
+    }
+
+    for (let i = starNumber; i < 5; i++) {
+        starGroup[i].classList.remove("fas");
+        starGroup[i].classList.add("far");
+    }
+}
